9dbf51d5e40c2d51f85648b9ddc9c0d3fc45285d
Ramakeerthi <ramakeerthi09@gmail.com>
2024-03-21 13:55:17 +0530

    Added Restful import, mapped get to a class function that returns a test database object

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello World!</h1>'
+testDB = {
+    1:{'name':'Clean car'},
+    2:{'name':'Write blog'},
+    3:{'name':'Start stream'},
+}
+
+class Items(Resource):
+    def get(self):
+        return testDB
+
+api.add_resource(Items, '/')
+
+# Basic route
+# @app.route('/')
+# def hello():
+#     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
